6c66087d8326929ee9e04973e70cbe9a25852fcb
imran122 <imransardar122@gmail.com>
2023-12-27 06:35:54 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-green-800 font-bold underline">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
